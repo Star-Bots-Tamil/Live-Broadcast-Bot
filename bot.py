4fda3d4f98c3d8fb2cad1be32b1ee5b946f94d20
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-12-14 07:03:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -130,33 +130,29 @@ async def get_id(event):
     if not chat:
         return
 
-    if isinstance(chat, (PeerUser, PeerChannel, PeerChat)):
-        chat_id = chat.id
+    if isinstance(chat, PeerUser):  # Private chat with the bot
+        await event.respond(f"💁🏻 Your ID is: `{chat.user_id}`", parse_mode='markdown')
 
-        if isinstance(chat, PeerUser):  # Private chat with the bot
-            await event.respond(f"💁🏻 Your ID is: `{chat_id}`", parse_mode='markdown')
-        else:
-            result = f"👥 Chat ID: `{chat_id}`\n"
-            
-            if isinstance(chat, PeerChat) and chat.message_thread_id:
-                result += f"💬 Forum/Topic ID: `{chat.message_thread_id}`\n"
+    result = f"👥 Chat ID: `{chat.id}`\n"
+    if isinstance(chat, PeerChat) and chat.message_thread_id:
+        result += f"💬 Forum/Topic ID: `{chat.message_thread_id}`\n"
 
-            if event.reply_to_msg_id:
-                reply_message = await event.get_reply_message()
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
 
-                if reply_message.forward.sender_id:  # Forwarded user
-                    sender_id = reply_message.forward.sender_id
-                    forwarder_id = reply_message.sender_id
-                    result += f"💁🏻 Original Sender (ID: `{sender_id}`)\n"
-                    result += f"⏩ Forwarder (ID: `{forwarder_id}`)"
+        if reply_message.forward.sender_id:  # Forwarded user
+            sender = await reply_message.forward.sender_id
+            forwarder = reply_message.sender_id
+            result += f"💁🏻 Original Sender ({get_display_name(sender)}), ID: `{sender}`\n"
+            result += f"⏩ Forwarder ({get_display_name(forwarder)}), ID: `{forwarder}`"
 
-                if reply_message.forward.chat_id:  # Forwarded channel
-                    channel_id = reply_message.forward.chat_id
-                    forwarder_id = reply_message.sender_id
-                    result += f"💬 Channel (ID: `{channel_id}`)\n"
-                    result += f"⏩ Forwarder (ID: `{forwarder_id}`)"
+        if reply_message.forward.chat_id:  # Forwarded channel
+            channel = await datgbot.get_entity(reply_message.forward.chat_id)
+            forwarder = reply_message.sender_id
+            result += f"💬 Channel {channel.title} ID: `{channel.id}`\n"
+            result += f"⏩ Forwarder ({get_display_name(forwarder)}), ID: `{forwarder}`"
 
-            await event.respond(result, parse_mode='markdown')
+    await event.respond(result, parse_mode='markdown')
 
 # First Forward 
 async def replace_links_in_message(message):
